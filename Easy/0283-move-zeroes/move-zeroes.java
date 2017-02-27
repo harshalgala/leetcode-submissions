86028f280a39b79547733bdfd4241b9d442aecab
Harshal Gala <harshalbgala@gmail.com>
2017-02-27 06:11:23 +0000

    Time: 1 ms (99.89%) | Memory: N/A - LeetX Squads

---
+++ Easy/0283-move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int count =0 ;
+        int i;
+        for (i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[count++]=nums[i];
+            }
+        }
+            while(count<nums.length)
+                nums[count++]=0;
+    }
+}
