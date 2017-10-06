7b8f921305fec08d3d50e74539f215dc1eaf7746
Harshal Gala <harshalbgala@gmail.com>
2017-10-06 20:42:48 +0000

    Time: 62 ms (6.66%) | Memory: N/A - LeetX Squads

---
+++ Easy/0415-add-strings/add-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        first = int(num1)
+        second = int(num2)
+        
+        return str(first + second)
