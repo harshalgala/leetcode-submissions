bb29d6489f82934559eaffef174f5603c929b0a4
Harshal Gala <harshalbgala@gmail.com>
2025-05-31 23:06:13 +0000

    Time: 4 ms (63.68%) | Memory: 17.8 MB (100.00%) - LeetX Squads

---
+++ Easy/0013-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romToInt = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        # Convert exception to literal values.
+        s = s.replace('IV','IIII')
+        s = s.replace('IX','VIIII')
+        s = s.replace('XL','XXXX')
+        s = s.replace('XC','LXXXX')
+        s = s.replace('CD','CCCC')
+        s = s.replace('CM','DCCCC')
+        total = 0
+        for char in s:
+            total += romToInt[char]
+        return total
